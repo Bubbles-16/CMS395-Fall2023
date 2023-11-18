92a6dfb920264f3b38eee4f289ceacf2d8a0923d
James McIntyre <98340552+JamesMcIntyre123@users.noreply.github.com>
2023-11-18 18:07:24 -0500

    Trigger Disappear

---
+++ EscapeRoom/Assets/trigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+
+    CircleCollider2D key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        key = GetComponent<CircleCollider2D>();
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.CompareTag("player")) {
+            gameObject.SetActive(false);
+        }
+    }
+}
